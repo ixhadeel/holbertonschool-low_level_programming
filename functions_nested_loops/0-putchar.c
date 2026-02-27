5a00dc595f9b5e2a8609bb230536ecd0068ab09d
ixhadel <hadeel.alqhtani206@gmail.com>
2026-02-27 00:20:19 +0000

    Task 0: print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
