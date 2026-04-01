bb8363bb275651b823386423a1a6f024679ca2ce
ixhadee <hadeel.alqhtani206@gmail.com>
2026-04-01 14:28:07 +0000

    Update dog.h with current prototypes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Basic structure for dog data
+ * Description: Basic structure to store dog attributes
  */
 struct dog
 {
@@ -16,14 +16,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* DOG_H */
