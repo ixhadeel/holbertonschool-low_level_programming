d79d5c12c25a4a5faffab542b4da717a694a5b1e
ixhadeel <hadeel.alqhtani206@gmail.com>
2026-04-09 15:10:54 +0000

    Task 3: To be is a to be the value of a variable

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,9 +4,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct printer - Struct printer
+ * @symbol: The symbol representing a data type
+ * @print: The function pointer to a print function
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int _putchar(char c);
 
 #endif /* VARIADIC_FUNCTIONS_H */
