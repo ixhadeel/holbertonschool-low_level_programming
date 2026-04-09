24df451e65bbbb5bcb8c9e2056a5985f378eac1f
ixhadeel <hadeel.alqhtani206@gmail.com>
2026-04-09 15:05:02 +0000

    Task 1: print numbers

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,8 +2,10 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 
 #endif /* VARIADIC_FUNCTIONS_H */
