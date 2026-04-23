f3ee9c821b9fafcffe00c38c0266fb986749d966
ixhadeel <hadeel.alqhtani206@gmail.com>
2026-04-23 13:05:51 +0000

    Fix: add 2-add_dnodeint.c and update header

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,31 +1,30 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the start of a doubly linked list
- * @head: double pointer to the start of the list
- * @n: the number to put in the new node
+ * add_dnodeint - adds a node to the beginning of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer to add in the node
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	/* 1. Create space for the new node */
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	/* 2. Put the data in and set the pointers */
 	new->n = n;
-	new->prev = NULL;   /* because it's going to be the first one */
-	new->next = *head;  /* it points to the old head */
+	new->prev = NULL;
+	new->next = *head;
 
-	/* 3. Important: if the list wasn't empty, update the old head's prev */
 	if (*head != NULL)
 		(*head)->prev = new;
 
-	/* 4. Move the head to point to our new node */
 	*head = new;
 
 	return (new);
