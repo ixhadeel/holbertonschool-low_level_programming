4b65d6f159fd94583830916852b15076261c8598
ixhadeel <hadeel.alqhtani206@gmail.com>
2026-04-23 13:43:59 +0000

    Fixed Betty warning: line length

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * create_file - creates a file with specific permissions
+ * create_file - creates a file
  * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * @text_content: text to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
@@ -14,19 +14,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* 1. Calculate length of text_content if it's not NULL */
 	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
 	}
 
-	/* 2. Open file: Create it, if exists Truncate it. Permissions: rw------- (0600) */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	/* 3. Write content if there is any */
 	if (text_content != NULL)
 	{
 		n_wrote = write(fd, text_content, len);
