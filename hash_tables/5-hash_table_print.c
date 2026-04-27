359ac15c8140e272f1518a0d15f19adbfdc4d9a0
ixhadee <hadeel.alqhtani206@gmail.com>
2026-04-27 14:28:14 +0000

    Implement hash_table_print function

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", node->key, node->value);
+			comma_flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
