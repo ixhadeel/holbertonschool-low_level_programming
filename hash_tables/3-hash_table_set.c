137df98581ecc40046798e9ce1c4986170324891
ixhadee <hadeel.alqhtani206@gmail.com>
2026-04-27 14:07:56 +0000

    Fix Betty warnings for task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add/update
+ * @key: the key, cannot be empty
+ * @value: the value associated with the key
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *tmp;
+	char *v_copy;
+	unsigned long int idx;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	v_copy = strdup(value);
+	if (!v_copy)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = v_copy;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		free(v_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = v_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
